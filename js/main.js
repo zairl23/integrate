4c63c48fda780cb45c4f4cee493841a0fb2e9402
zairl23 <756127792@qq.com>
2014-02-27 03:35:24 +0800

    增加对ui的文字说明

--- js/main.js
+++ js/main.js
@@ -3,25 +3,45 @@ $(document).ready(function(){
     var domainLocal = 'images/';
     var size  = '-ui.jpg';
     var sizeLocal = 'images/8meow.png';
-    var login = ['登录.jpg', '登录成功.jpg'];
-    var signup = ['注册.jpg'];
-    var home = ['首页(主题).jpg', '首页(后台上传).jpg'];
-    var relationship = ['关系页.jpg'];
-    var homeUser = ['个人页-基本信息.jpg', '个人页-设置.jpg'];
-    var video = ['拍摄.jpg', '拍摄-完成上传.jpg'];
-    var changePasswd = ['设置-修改密码.jpg'];
-    var forgetPasswd = ['忘记密码.jpg', '忘记密码-修改密码.jpg', '忘记密码-验证码.jpg'];
-    var changePhonenumber = ['设置-修改手机号.jpg', '设置-修改手机号-验证码.jpg'];
-    var qa = ['设置-Q&A.jpg'];
-    var invite = ['设置-好友邀请.jpg'];
-    var account = ['设置-帐号安全.jpg'];
+    var login = [
+            {"view": "登录.jpg", "desc":""},
+            {"view": "登录成功.jpg", "desc":"登录成功"},        
+        ];
+    var signup = [{"view": "注册.jpg", "desc":"描述显示在这里"}];
+    var home = [
+            {"view": "首页(主题).jpg", "desc":"描述显示在这里"},
+            {"view": "首页(后台上传).jpg", "desc":"允许双线程上传"}
+        ];
+    var relationship = [{"view": "关系页.jpg", "desc":"长按关注用户信息区域弹出取消关注提示，长按粉丝用户信息区域弹出关注提示"}];
+    var homeUser = [
+            {"view":"个人页-基本信息.jpg", "desc":"点击头像弹出提示选择相册或相机"},
+            {"view":"个人页-设置.jpg", "desc":"点击版本信息跳转苹果商店页面"}
+        ];
+    var video = [
+            {"view":"拍摄.jpg", "desc":"为了能全屏看到拍摄画面，把屏幕的白色区域(上下左右)做成半透明的效果"},
+            {"view":"拍摄-完成上传.jpg", "desc":"点击缩略图播放预览8meow,未填写标题和未选择主题，上传图标为灰色，表示无法上传"}
+        ];
+    var changePasswd = [{"view":"设置-修改密码.jpg", "desc":"点击发送验证码按钮，发送成功，按钮切换为验证码输入框"}];
+    var forgetPasswd = [
+            {"view":"忘记密码.jpg", "desc":"描述显示在这里"},
+            {"view":"忘记密码-验证码.jpg", "desc":"some"},
+            {"view":"忘记密码-修改密码.jpg", "desc":"some"}
+            
+        ];
+    var changePhonenumber = [
+            {"view":"设置-修改手机号.jpg","desc":"描述显示在这里"},
+            {"view":"设置-修改手机号-验证码.jpg", "desc":"描述显示在这里"}
+        ];
+    var qa = [{"view":"设置-Q&A.jpg","desc":"用邮件的形式发送问题到官方邮箱，下方列出常见问题列表，从服务端获取"}];
+    var invite = [{"view":"设置-好友邀请.jpg","desc":"如果好友号码在电话薄，直接进入电话簿选择"}];
+    var account = [{"view":"设置-帐号安全.jpg","desc":"描述显示在这里"}];
     var uis = ['login', 'signup', 'home', 'relationship', 'homeUser', 'video', 'changePasswd','forgetPasswd', 'changePhonenumber', 'qa', 'invite', 'account'];
 
     // convert path to img's src    
     function pathToUrl(path) {
         var imgs = "";
         for(var i=0; i < path.length; i++) {
-            var imgs = imgs + "<img title=" + path[i] + " src=" + domain + path[i] + size + " />";         
+            var imgs = imgs + "<img title=" + path[i].desc + " src=" + domain + path[i].view + size + " />";         
         }
         return imgs;
     }
