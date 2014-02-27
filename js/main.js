178c6cbb11c8bf564514c5ba6a01e57d2bbdc78f
zairl23 <756127792@qq.com>
2014-02-27 16:49:58 +0800

    设置个人主页

--- js/main.js
+++ js/main.js
@@ -13,10 +13,14 @@ $(document).ready(function(){
             {"view": "首页(后台上传).jpg", "desc":"允许双线程上传"}
         ];
     var relationship = [{"view": "关系页.jpg", "desc":"长按关注用户信息区域弹出取消关注提示，长按粉丝用户信息区域弹出关注提示"}];
-    var homeUser = [
-            {"view":"个人页-基本信息.jpg", "desc":"点击头像弹出提示选择相册或相机"},
-            {"view":"个人页-设置.jpg", "desc":"点击版本信息跳转苹果商店页面"}
-        ];
+     var homeUser = [
+            {"view":"个人主页-无视频.jpg", "desc":"用户没有发布过视频"},
+            {"view":"个人主页-有视频.jpg", "desc":"用户有发布过视频"}
+     ];
+    var setting = [
+            {"view":"设置-修改头像用户名.jpg", "desc":"点击头像弹出提示选择相册或相机"},
+            {"view":"设置-更多.jpg", "desc":"点击版本信息跳转苹果商店页面"}
+     ];
     var video = [
             {"view":"拍摄.jpg", "desc":"为了能全屏看到拍摄画面，把屏幕的白色区域(上下左右)做成半透明的效果"},
             {"view":"拍摄-完成上传.jpg", "desc":"点击缩略图播放预览8meow,未填写标题和未选择主题，上传图标为灰色，表示无法上传"}
@@ -35,7 +39,7 @@ $(document).ready(function(){
     var qa = [{"view":"设置-Q&A.jpg","desc":"用邮件的形式发送问题到官方邮箱，下方列出常见问题列表，从服务端获取"}];
     var invite = [{"view":"设置-好友邀请.jpg","desc":"如果好友号码在电话薄，直接进入电话簿选择"}];
     var account = [{"view":"设置-帐号安全.jpg","desc":"描述显示在这里"}];
-    var uis = ['login', 'signup', 'home', 'relationship', 'homeUser', 'video', 'changePasswd','forgetPasswd', 'changePhonenumber', 'qa', 'invite', 'account'];
+    var uis = ['login', 'signup', 'home', 'relationship', 'homeUser','setting', 'video', 'changePasswd','forgetPasswd', 'changePhonenumber', 'qa', 'invite', 'account'];
 
     // convert path to img's src    
     function pathToUrl(path) {
