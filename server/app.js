bc53787060564cb63a038a21fecad074dcccae2e
zairl23 <756127792@qq.com>
2014-03-13 23:23:02 +0800

    add playlist

--- server/app.js
+++ server/app.js
@@ -7,6 +7,17 @@ var koa = require('koa')
 app.use(router(app));
 // server static file
 app.use(serve('.'));
+//app.use('/', )
+app.get('/web', function *(next) {
+  //var user = yield User.findOne(this.params.id);
+  this.body = 'hello';
+});
+app.get('/web/1', function *(next) {
+  //var user = yield User.findOne(this.params.id);
+  this.body = 'hello';
+  //app.use(serve('index.html'));
+});
+
 
 var port = Number(process.env.PORT || 3000);
 app.listen(port);
